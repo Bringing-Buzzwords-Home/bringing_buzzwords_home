82b406966264b5a10d29aefd4be253e355c0d5b1
JonathanKross <kross.jonathan+github@gmail.com>
2016-06-01 16:41:48 -0400

    Registering models in Admin.py

--- visualize/admin.py
+++ visualize/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import County, GuardianCounted, Geo, Item, Station
 
-# Register your models here.
+
+admin.site.register(County)
+admin.site.register(GuardianCounted)
+admin.site.register(Geo)
+admin.site.register(Item)
+admin.site.register(Station)
