d0f2be3996ae8c304bc75e745bd7a95b3a11228e
gcrowder <gregory.j.crowder@gmail.com>
2016-06-03 11:28:12 -0400

    Update index page with links to 50 states and DC

--- visualize/views.py
+++ visualize/views.py
@@ -1,12 +1,14 @@
+import operator
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import County, GuardianCounted, Geo, Item, Station
 from .utilities import draw_state_deaths, states
 
 
-
 def index(request):
-    return render(request, "visualize/index.html")
+    state_list = sorted(list(states.items()), key=operator.itemgetter(1))
+    context = {'states': state_list}
+    return render(request, "visualize/index.html", context)
 
 
 def state(request, state):
