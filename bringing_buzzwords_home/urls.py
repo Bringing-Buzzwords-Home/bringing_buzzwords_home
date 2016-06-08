a186a29ab028e601bd1f3dff0d37e09e412a7379
gcrowder <gregory.j.crowder@gmail.com>
2016-06-08 15:57:34 -0400

    Fix the trailing slash bug.

--- bringing_buzzwords_home/urls.py
+++ bringing_buzzwords_home/urls.py
@@ -11,8 +11,8 @@ router.register(r'state', views.StateViewSet)
 
 urlpatterns = [
     url(r'^$', views.index, name='homepage'),
-    url(r'^state/(?P<state>\D+)$', views.state, name='state'),
-    url(r'^json/(?P<state>\D+)$', views.state_json, name='state_json'),
+    url(r'^state/(?P<state>\D+)/$', views.state, name='state'),
+    url(r'^json/(?P<state>\D+)/$', views.state_json, name='state_json'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^county/(?P<county>\d+)$', views.county, name='county'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
