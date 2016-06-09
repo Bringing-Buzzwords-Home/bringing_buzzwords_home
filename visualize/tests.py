9531042b840253a78b69a72859be22d84b95af0e
gcrowder <gregory.j.crowder@gmail.com>
2016-06-09 10:59:08 -0400

    Wrote tests for County model. They don't work.

--- visualize/tests.py
+++ visualize/tests.py
@@ -1,7 +1,54 @@
 from django.test import TestCase
-from .models import County, State, GuardianCounted, Item, Crime, State
-# Create your tests here.
+from .models import County, State, GuardianCounted, Item, Crime
+
 
 class CountyTestCase(TestCase):
     def setup(self):
-        County.objects.create()
+        cali = County(county_name='Orange County',
+                      state='California',
+                      FIPS='06059',
+                      pop_est_2015=9999999,
+                      pop_est_2014=8888888,
+                      pop_est_2013=7777777,
+                      pop_est_2012=6666666,
+                      pop_est_2011=5555555,
+                      pop_est_2010=4444444,
+                      pop_est_2009=3333333,
+                      pop_est_2008=2222222,
+                      pop_est_2007=1111111,
+                      pop_est_2006=999999,
+                      pop_est_2005=888888,
+                      pop_est_2004=777777,
+                      pop_est_2003=666666,
+                      pop_est_2002=555555,
+                      pop_est_2001=444444,
+                      pop_est_2000=333333,
+                      google_county_name='Orange County')
+        carolina = County(county_name='Orange County',
+                          state='North Carolina',
+                          FIPS='37135',
+                          pop_est_2015=9999999,
+                          pop_est_2014=8888888,
+                          pop_est_2013=7777777,
+                          pop_est_2012=6666666,
+                          pop_est_2011=5555555,
+                          pop_est_2010=4444444,
+                          pop_est_2009=3333333,
+                          pop_est_2008=2222222,
+                          pop_est_2007=1111111,
+                          pop_est_2006=999999,
+                          pop_est_2005=888888,
+                          pop_est_2004=777777,
+                          pop_est_2003=666666,
+                          pop_est_2002=555555,
+                          pop_est_2001=444444,
+                          pop_est_2000=333333,
+                          google_county_name='Orange County')
+        cali.save()
+        carolina.save()
+
+    def test_counties_have_attributes(self):
+        the_oc = County.objects.get(state='California', county_name='Orange County')
+        chapel_hill = County.objects.get(state='North Carolina', google_county_name='Orange_County')
+        self.assertEqual(the_oc.FIPS, '06059')
+        self.assertEqual(chapel_hill.pop_est_2014, 8888888)
